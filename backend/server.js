0ab6b42acfac30218c5869c00d6ed00c42ed8574
prathyusha-padmanabhuni <71263548+prathyusha-padmanabhuni@users.noreply.github.com>
2022-04-19 22:19:09 +0530

    Revert "Master"

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
 require('dotenv').config();
+var path=require('path')
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-// app.use('/public',express.static("C:/Users/riktam/Desktop/eventmngmnt/public"));
+app.use('/public',express.static("C:/Users/riktam/Desktop/eventmngmnt/public"));
 // console.log(path.join(__dirname+"/public"))
-// app.use(express.static(path.join(__dirname , "/../public")))
-// console.log(path.join(__dirname , "/../public"))
-app.use('/public',express.static("./client/public"));
+// app.use(express.static(path.join(__dirname + "public")))
+// app.use('/public',express.static("/../..\\/public"));
 const uri ="mongodb+srv://admin-prathyu:Prathyusha123@cluster0.l7q0r.mongodb.net/eventmgt";
  
 mongoose.connect(uri,
@@ -27,14 +28,6 @@ const usersubRouter = require('./routes/usersubrouter');
 app.use('/users', usersRouter);
 app.use('/main', usersmainRouter);
 app.use('/sub', usersubRouter);
-if (process.env.NODE_ENV === 'production') {
-    // Set Static Folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-};
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
