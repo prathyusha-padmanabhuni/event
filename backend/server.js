d89442fbe63f3c4dc2940bfebe5040d1995f4dab
unknown <pratthyushasindhusha@gmail.com>
2022-04-19 11:33:40 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/public',express.static("C:/Users/riktam/Desktop/eventmngmnt/public"));
-const uri = process.env.ATLAS_URI;
+const uri ="mongodb+srv://admin-prathyu:Prathyusha123@cluster0.l7q0r.mongodb.net/eventmgt";
  
 mongoose.connect(uri,
     err => {
